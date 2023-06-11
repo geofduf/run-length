a1048c51604f86ec8dd5762e194948c385c93f6b
Geof <46729592+geofduf@users.noreply.github.com>
2023-06-11 18:04:19 +0200

    add documentation

---
+++ sequence/doc.go
@@ -0,0 +1,21 @@
+/*
+Package sequence implements run-length encoding of regularly spaced binary states.
+It defines the type Sequence, with methods for adding and querying values and
+the type Store, with methods for interacting with a collection of sequences.
+
+A Sequence is defined by a starting timestamp, a frequency, and a maximum length.
+It follows an append-only pattern and supports automatic discarding of oldest values when
+its capacity is reached. It can be exported as []byte, easing integration with
+storage systems.
+
+Sequence values are represented as uint8. The only supported values are:
+
+  const (
+    StateInactive uint8 = iota // 0b00
+    StateActive                // 0b01
+    StateUnknown               // 0b10
+  )
+
+Passing unsupported values to functions or methods will result in undefined behavior.
+*/
+package sequence

c12bff6d212cae36e175c583ad3427e311f4c70b
Geof <46729592+geofduf@users.noreply.github.com>
2023-08-21 18:21:58 +0200

    add support for Unix time

--- sequence/serializer.go
+++ sequence/serializer.go
@@ -25,14 +25,15 @@ const (
 // serialize is a convenience function that returns a JSON encoding of the time series
 // using layout as time layout, loc as time location, n as precision level for
 // float values and flag to define which values to include in the serialized output.
+// As a special case, if layout is an empty string time values will be represented
+// as Unix times instead of textual representations. In that case, loc is not used.
 func serialize(q QuerySet, layout string, loc *time.Location, n int, flag int) []byte {
 	if len(q.Count) == 0 {
 		return []byte("[]")
 	}
-	layout = `"` + layout + `"`
 	var count, sum, mean bool
 	var rowNull string
-	approxRowSize := 10 + len(layout)
+	approxRowSize := 10
 	if flag&SerializeCount != 0 {
 		rowNull += serializerCountPrefix + "0"
 		approxRowSize += 14
@@ -49,12 +50,29 @@ func serialize(q QuerySet, layout string, loc *time.Location, n int, flag int) [
 		mean = true
 	}
 	rowNull += serializerRowSuffix
+	var formattedTime bool
+	var t time.Time
+	var ts int64
+	if layout != "" {
+		formattedTime = true
+		layout = `"` + layout + `"`
+		t = time.Unix(q.Timestamp, 0).In(loc)
+		approxRowSize += len(layout)
+	} else {
+		ts = q.Timestamp
+		approxRowSize += 10
+	}
 	buf := make([]byte, 0, 2+len(q.Count)*approxRowSize)
-	t := time.Unix(q.Timestamp, 0).In(loc)
 	buf = append(buf, serializerBasePrefix)
 	for i := 0; i < len(q.Count); i++ {
 		buf = append(buf, serializerRowPrefix...)
-		buf = append(buf, t.Format(layout)...)
+		if formattedTime {
+			buf = append(buf, t.Format(layout)...)
+			t = t.Add(time.Duration(q.Frequency) * time.Second)
+		} else {
+			buf = strconv.AppendInt(buf, ts, 10)
+			ts += q.Frequency
+		}
 		if q.Count[i] == 0 {
 			buf = append(buf, rowNull...)
 		} else {
@@ -72,7 +90,6 @@ func serialize(q QuerySet, layout string, loc *time.Location, n int, flag int) [
 			}
 			buf = append(buf, serializerRowSuffix...)
 		}
-		t = t.Add(time.Duration(q.Frequency) * time.Second)
 	}
 	buf[len(buf)-1] = serializerBaseSuffix
 	return buf
@@ -81,6 +98,8 @@ func serialize(q QuerySet, layout string, loc *time.Location, n int, flag int) [
 // Serialize is a convenience method that returns a JSON encoding of the time series
 // using layout as time layout, loc as time location, n as precision level for
 // float values and flag to define which values to include in the serialized output.
+// As a special case, if layout is an empty string time values will be represented
+// as Unix times instead of textual representations. In that case, loc is not used.
 func (q QuerySet) Serialize(layout string, loc *time.Location, n int, flag int) []byte {
 	return serialize(q, layout, loc, n, flag)
 }
--- sequence/serializer_test.go
+++ sequence/serializer_test.go
@@ -43,6 +43,19 @@ func TestSerialize(t *testing.T) {
 				"]",
 			},
 		},
+		{
+			3,
+			"",
+			2,
+			SerializeSum | SerializeMean,
+			[]string{
+				"[",
+				`{"date":946782245,"sum":5,"mean":1.00},`,
+				`{"date":946782545,"sum":null,"mean":null},`,
+				`{"date":946782845,"sum":1,"mean":0.25}`,
+				"]",
+			},
+		},
 	}
 	for _, tt := range tests {
 		got := q.Serialize(tt.layout, time.UTC, tt.precision, tt.flag)
